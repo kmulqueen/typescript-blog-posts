85bb26cae585945f7b5bb9bc019b715ec1d6d45f
Kyle Mulqueen <kmulqdrums710@gmail.com>
2025-05-18 12:26:54 -0700

    error handling

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import type { BlogPost } from "./components/BlogPosts";
 import { get } from "./util/http";
 import fetchingImg from "./assets/data-fetching.png";
 import BlogPosts from "./components/BlogPosts";
+import ErrorMessage from "./components/ErrorMessage";
 
 type RawDataBlogPost = {
   id: number;
@@ -14,21 +15,35 @@ type RawDataBlogPost = {
 
 function App() {
   const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState<string>("");
 
   useEffect(() => {
     async function fetchPosts() {
-      const posts = await get<RawDataBlogPost[]>(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
+      try {
+        setIsLoading(true);
+        const posts = await get<RawDataBlogPost[]>(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
 
-      const blogPosts: BlogPost[] = posts.map((rawPost) => {
-        return {
-          id: rawPost.id,
-          title: rawPost.title,
-          text: rawPost.body,
-        };
-      });
-      setFetchedPosts(blogPosts);
+        const blogPosts: BlogPost[] = posts.map((rawPost) => {
+          return {
+            id: rawPost.id,
+            title: rawPost.title,
+            text: rawPost.body,
+          };
+        });
+        setFetchedPosts(blogPosts);
+        setIsError("");
+      } catch (error) {
+        if (error instanceof Error) {
+          setIsError(error.message);
+        } else {
+          setIsError("Failed to fetch posts.");
+        }
+      } finally {
+        setIsLoading(false);
+      }
     }
 
     fetchPosts();
@@ -39,6 +54,13 @@ function App() {
   if (fetchedPosts) {
     content = <BlogPosts posts={fetchedPosts} />;
   }
+  if (isError) {
+    content = <ErrorMessage text={isError} />;
+  }
+  if (isLoading) {
+    content = <p id="loading-fallback">Loading...</p>;
+  }
+
   return (
     <main>
       <img
