6ef32b8421dfe5f88744c57a06714f5d1315cd0b
Kyle Mulqueen <kmulqdrums710@gmail.com>
2025-05-17 22:02:37 -0700

    created get http util function. fetch dummy posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,53 @@
+import { useEffect, useState } from "react";
+import type { ReactNode } from "react";
+import type { BlogPost } from "./components/BlogPosts";
+import { get } from "./util/http";
+import fetchingImg from "./assets/data-fetching.png";
+import BlogPosts from "./components/BlogPosts";
+
+type RawDataBlogPost = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};
+
 function App() {
-  return <h1>Data Fetching!</h1>;
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const posts = (await get(
+        "https://jsonplaceholder.typicode.com/posts"
+      )) as RawDataBlogPost[];
+
+      const blogPosts: BlogPost[] = posts.map((rawPost) => {
+        return {
+          id: rawPost.id,
+          title: rawPost.title,
+          text: rawPost.body,
+        };
+      });
+      setFetchedPosts(blogPosts);
+    }
+
+    fetchPosts();
+  }, []);
+
+  let content: ReactNode;
+
+  if (fetchedPosts) {
+    content = <BlogPosts posts={fetchedPosts} />;
+  }
+  return (
+    <main>
+      <img
+        src={fetchingImg}
+        alt="Abstract image depicting data fetching process."
+      />
+      {content}
+    </main>
+  );
 }
 
 export default App;
---
+++ src/util/http.ts
@@ -0,0 +1,17 @@
+/**
+ * HTTP GET request.
+ *
+ * @export
+ * @param {string} url
+ * @return data
+ */
+export async function get(url: string) {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data.");
+  }
+
+  const data = response.json() as unknown;
+  return data;
+}
